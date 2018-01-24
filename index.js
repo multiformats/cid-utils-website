b34c9043f162a611919bc97f02909ad3be9d3832
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-01-24 16:42:12 +0100

    Add multihash info

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const CID = require('cids')
+const multihash = require('multihashes')
+// const multibase = require('multibase')
 
 function decodeCID (inputCid) {
   const decoded = new CID(inputCid)
   return decoded.toJSON()
 }
 
-function setOutput (output, value) {
-  window.location.hash = value
-  try {
-    output.innerText = JSON.stringify(decodeCID(value), null, 2)
-  } catch (err) {
-    output.innerText = err
-  }
-}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const output = document.querySelector('#output')
+  const output = document.querySelector('#cid')
   const input = document.querySelector('#input-cid')
+  const multihashOutput = document.querySelector('#multihash')
+  const multibaseOutput = document.querySelector('#multibase')
+
+  function setOutput (output, value) {
+    window.location.hash = value
+    try {
+      const cid = decodeCID(value)
+      // cidv0 ::= <multihash-content-address>
+      // QmRds34t1KFiatDY6yJFj8U9VPTLvSMsR63y7qdUV3RMmT
+      // <cidv1> ::= <multibase-prefix><cid-version><multicodec-packed-content-type><multihash-content-address>
+      // zb2rhiVd5G2DSpnbYtty8NhYHeDvNkPxjSqA7YbDPuhdihj9L
+      // if (cid.version === 0) {
+      //   // console.log(multibase.decode(cid.version))
+      //   //base58btc
+      // }
+      // if (cid.version === 1) {
+      //   // 
+      // }
+      const mh = multihash.decode(cid.hash)
+      output.innerText = `Codec: ${cid.codec} Version: ${cid.version}`
+      multihashOutput.innerText = `Code: ${mh.code} Hash Function: ${mh.name} Length: ${mh.length}`
+    } catch (err) {
+      output.innerText = err
+    }
+  }
+
   if (input.value) {
     setOutput(output, input.value.trim())
   }
