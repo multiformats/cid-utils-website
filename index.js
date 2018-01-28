24c3988508ddf911eb63869e2aee44d6fd3e7980
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-01-28 02:13:39 +0100

    Show which multibase is being used

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const CID = require('cids')
 const multihash = require('multihashes')
 // const multibase = require('multibase')
+const multibases = require('multibase/src/constants')
 
 function decodeCID (inputCid) {
   const decoded = new CID(inputCid)
   return decoded.toJSON()
 }
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const output = document.querySelector('#cid')
   const input = document.querySelector('#input-cid')
   const multihashOutput = document.querySelector('#multihash')
@@ -22,13 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
       // QmRds34t1KFiatDY6yJFj8U9VPTLvSMsR63y7qdUV3RMmT
       // <cidv1> ::= <multibase-prefix><cid-version><multicodec-packed-content-type><multihash-content-address>
       // zb2rhiVd5G2DSpnbYtty8NhYHeDvNkPxjSqA7YbDPuhdihj9L
-      // if (cid.version === 0) {
-      //   // console.log(multibase.decode(cid.version))
-      //   //base58btc
-      // }
-      // if (cid.version === 1) {
-      //   // 
-      // }
+      if (cid.version === 0) {
+        multibaseOutput.innerText = `Multibase: base58btc`
+      }
+      if (cid.version === 1) {
+        const code = value.substring(0, 1)
+        const multibaseCode = multibases.codes[code]
+        multibaseOutput.innerText = `Multibase: ${multibaseCode.name}`
+      }
       const mh = multihash.decode(cid.hash)
       output.innerText = `Codec: ${cid.codec} Version: ${cid.version}`
       multihashOutput.innerText = `Code: ${mh.code} Hash Function: ${mh.name} Length: ${mh.length}`
@@ -40,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (input.value) {
     setOutput(output, input.value.trim())
   }
-  if (window.location.hash !== "") {
+  if (window.location.hash !== '') {
     setOutput(output, window.location.hash.substr(1))
     input.value = window.location.hash.substr(1)
   }
